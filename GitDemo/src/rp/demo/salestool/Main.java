c755d8321121017fb6386374553acefb09ee480e
atranrp <atran@resultspositive.com>
2016-02-03 15:04:34 -0600

    Initial checkin of sales tool

---
+++ GitDemo/src/rp/demo/salestool/Main.java
@@ -0,0 +1,20 @@
+package rp.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 SalesData data = new SalesData();
+		 
+		 displayGreeting();
+		 data.display();
+     
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Hello");
+		System.out.println("This app shows sales data");
+		
+		
+	}
+
+}
---
+++ GitDemo/src/rp/demo/salestool/SalesData.java
@@ -0,0 +1,12 @@
+package rp.demo.salestool;
+
+public class SalesData {
+
+	
+	int data [] = {0,4,42} ;
+	public  void display() {
+	 System.out.print("TODO: Implement Data Display");
+
+	}
+
+}
